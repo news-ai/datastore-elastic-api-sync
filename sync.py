cf0c4e41f1afad54a9403e804e20c5723ffc1229
Abhi Agarwal <me@abhiagarwal.com>
2016-08-26 16:48:12 -0400

    Finished datastore->ES sync for publications and agencies

--- sync.py
+++ sync.py
@@ -1,5 +1,6 @@
 # Stdlib imports
 import urllib3
+import os
 import json
 from datetime import datetime
 
@@ -8,6 +9,13 @@ import requests
 import certifi
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from elasticsearch import Elasticsearch, helpers
+from gcloud import datastore
+
+# Elasticsearch
+ELASTICSEARCH_USER = os.environ['NEWSAI_ELASTICSEARCH_USER']
+ELASTICSEARCH_PASSWORD = os.environ['NEWSAI_ELASTICSEARCH_PASSWORD']
+
+client = datastore.Client('newsai-1166')
 
 # Removing requests warning
 urllib3.disable_warnings()
@@ -24,63 +32,33 @@ es = Elasticsearch(
 )
 
 
-def sync_entities_es(new_index_name, entities):
-    if entities:
-        to_append = []
-        for entity in entities:
-            doc = {
-                '_type': 'entity',
-                '_index': new_index_name,
-                'data': entity
-            }
-            to_append.append(doc)
-        res = helpers.bulk(es, to_append)
-        print res
+def sync_es(index_name, kind, result_type):
+    to_append = []
+    query = client.query(kind=kind)
+    for result in query.fetch():
+        doc = {
+            '_type': result_type,
+            '_index': index_name,
+            'data': result
+        }
+        to_append.append(doc)
+    res = helpers.bulk(es, to_append)
+    print res
 
 
-def create_alias():
-    es.indices.put_alias(index='entities', name='entity')
+def create_alias(index, name):
+    es.indices.put_alias(index=index, name=name)
 
 
-def change_alias(new_index_name):
-    es.indices.delete_alias(index='_all', name='entity')
-    es.indices.put_alias(index=new_index_name, name='entity')
+def reset_elastic(kind, name):
+    es.indices.delete(index=kind, ignore=[400, 404])
+    es.indices.create(index=kind, ignore=[400, 404])
+    create_alias(kind, name)
 
+# Publications
+# reset_elastic('publications_v1', 'publications')
+# sync_es('publications', 'Publication', 'publication')
 
-def delete_previous_index(previous_index):
-    es.indices.delete(index=previous_index, ignore=[400, 404])
-
-
-def get_newest_index():
-    indices = es.cat.indices(format='json')
-    for i in indices:
-        if 'entities' in i:
-            return i
-    return indices[2]
-
-
-def generate_next_index(index):
-    if 'v' in index:
-        index = index.split('_v')
-        index_number = int(index[1]) + 1
-        index = index[0] + '_v' + str(index_number)
-    else:
-        index = index + '_v1'
-    return index
-
-
-def deploy_new_update():
-    previous_index = get_newest_index()['index']
-    new_index = generate_next_index(previous_index)
-    es.indices.create(index=new_index, ignore=[400, 404])
-    get_entities(new_index)
-    change_alias(new_index)
-    delete_previous_index(previous_index)
-
-
-def reset_elastic():
-    es.indices.delete(index='entities', ignore=[400, 404])
-    es.indices.create(index='entities', ignore=[400, 404])
-    get_entities()
-
-deploy_new_update()
+# Agencies
+# reset_elastic('agencies_v1', 'agencies')
+# sync_es('agencies', 'Agency', 'agency')
