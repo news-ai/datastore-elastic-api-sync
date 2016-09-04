8cbb1fd00325f5d64364500d3f1f159d03dbd17a
Abhi Agarwal <me@abhiagarwal.com>
2016-09-03 20:05:02 -0400

    Method to sync only contacts from lists

--- sync.py
+++ sync.py
@@ -43,27 +43,67 @@ def sync_es(index_name, kind, result_type):
         result_id = result.key.id
         result['Id'] = int(result_id)
 
-        if 'CustomFields.Name' in result:
-            del result['CustomFields.Name']
-
-        if 'CustomFields.Value' in result:
-            del result['CustomFields.Value']
-
         doc = {
             '_type': result_type,
             '_index': index_name,
             'data': result
         }
+        to_append.append(doc)
         print limit
         if limit == 100:
             res = helpers.bulk(es, to_append)
             print res
             to_append = []
             limit = 0
-        to_append.append(doc)
         limit = limit + 1
         total = total + 1
         print total
+
+
+def sync_list_contacts():
+    query = client.query(kind='MediaList')
+    total = 0
+    for media_list in query.fetch():
+        if 'Contacts' in media_list:
+            to_append = []
+            limit = 0
+            for contact_id in media_list['Contacts']:
+                key = client.key('Contact', int(contact_id))
+                contact = client.get(key)
+
+                print contact
+
+                if 'CustomFields.Name' in contact:
+                    del contact['CustomFields.Name']
+                if 'CustomFields.Value' in contact:
+                    del contact['CustomFields.Value']
+
+                contact_id = contact.key.id
+                contact['Id'] = int(contact_id)
+
+                media_list_id = media_list.key.id
+                contact['ListId'] = int(media_list_id)
+
+                doc = {
+                    '_type': 'contact',
+                    '_index': 'contacts',
+                    'data': contact
+                }
+
+                to_append.append(doc)
+
+                if limit == 100:
+                    res = helpers.bulk(es, to_append)
+                    print res
+                    to_append = []
+                    limit = 0
+
+                limit = limit + 1
+
+            # If any left at the end
+            if len(to_append) > 0:
+                res = helpers.bulk(es, to_append)
+                print res
 
 
 def reset_elastic(kind):
@@ -80,4 +120,4 @@ def reset_elastic(kind):
 
 # Agencies
 # reset_elastic('contacts')
-# sync_es('contacts', 'Contact', 'contact')
+sync_list_contacts()
